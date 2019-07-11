f8330c6ee27523665a034681db6fa0b74d8c955e
matthewwyndham <matthewwyndham@gmail.com>
2019-07-11 16:03:14 -0600

    fully functional, ready for code review

--- HTTPHandler/HTTPHandler.cs
+++ HTTPHandler/HTTPHandler.cs
@@ -12,20 +12,25 @@ namespace CanvasAPIWrapper
 {
     partial class HTTPHandler
     {
-        public HttpClient client;
-        public int runners;
-        public int MaxRunners;
-        private int RunnerPenalty;
-        public float API_cost_limit;
-        public float API_upfront_cost;
-        public List<float> costs;
-        private string domain;
-        private string api_prefix;
-        private string api_context;
+        private HttpClient Client;
+        private int Runners;
+        private int MaxRunners;
+        private float APICostLimit;
+        private float APIUpfrontCost;
+        private List<float> Costs;
+        private string Domain;
+        private string APIPrefix;
+        private string APIContext;
+        private float BigCost;
+        public bool ShowRetries;
+        public int APIRetryLimit;
+        public int ErrorCount; // :T:
 
         public HTTPHandler(HttpClient c)
         {
-            client = c;
+            ShowRetries = false;
+            ErrorCount = 0; // :T:
+            Client = c;
 
             // Polly -> Retry (In case of any real errors)
             Policy
@@ -39,34 +44,43 @@ namespace CanvasAPIWrapper
                 });
 
             // Limit the number of concurrent API calls
-            API_cost_limit = 700F; // API rate limit is 700
-            API_upfront_cost = 15F; // canvas seems to automatically apply a cost of 15 to each
-            var big_cost = 12F; // this is the biggest API call I've ever seen
+            APICostLimit = 700F; // API rate limit is 700
+            APIUpfrontCost = 15F; // canvas seems to automatically apply a cost of 15 to each
+            BigCost = 12F; // this is the biggest API call I've ever seen
 
-            costs = new List<float>();
-            costs.Add(big_cost);
+            Costs = new List<float>();
+            Costs.Add(BigCost);
             
-            var small_call_max = (API_cost_limit / API_upfront_cost);
-            
-            MaxRunners = Convert.ToInt32((API_cost_limit - small_call_max * big_cost) / API_upfront_cost);
-            runners = 0;
-            RunnerPenalty = 1;
+            MaxRunners = 5; // start off small
+            Runners = 0;
+
+            APIRetryLimit = 10;
 
             // Ensure proper authentication
             string token = Environment.GetEnvironmentVariable("CANVAS_API_TOKEN");
             if (token == null) { throw new System.ArgumentException("Canvas api token was not set"); }
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
-            domain = "https://byui.instructure.com";
-            api_prefix = "/api/v1/";
-            api_context = domain + api_prefix;
+            Domain = "https://byui.instructure.com";
+            APIPrefix = "/api/v1/";
+            APIContext = Domain + APIPrefix;
         }
 
-        public void RecalculateMaxRunners(float cost, float limit)
+        // Set to "true" if you want to see how many retries you are making (defaults to "false")
+        public bool Show403Retries(bool n)
         {
-            costs.Add(cost);
-            var small_call_max = (API_cost_limit / API_upfront_cost);
-            MaxRunners = Convert.ToInt32(((API_cost_limit - small_call_max * costs.Average()) / API_upfront_cost) - RunnerPenalty);
+            ShowRetries = n;
+            return ShowRetries;
+        }
+
+        // determine how many concurrent API calls we can make, based on the 15-unit-holding-fee that Canvas has.
+        private void RecalculateMaxRunners(float cost, float limit)
+        {
+            // var old = MaxRunners; // :T:
+
+            Costs.Add(cost);
+            var SmallCallMax = (APICostLimit / APIUpfrontCost);
+            MaxRunners = Convert.ToInt32((APICostLimit - SmallCallMax * Costs.ToArray().Average()) / APIUpfrontCost);
 
             // API limit = 700 / 15 concurrent, so 46 concurrent calls plus some change.
             // Setting it to 45 max saves us if we hit a big call after a bunch of small ones.
@@ -74,45 +88,85 @@ namespace CanvasAPIWrapper
             if (MaxRunners < 5) MaxRunners = 5; // in case the penalty gets too large we don't want to stop.
 
             // the average quickly gets out of touch when it remembers the past
-            if (costs.Count > 30) costs.RemoveAt(0);
+            // if (Costs.Count > 30) Costs.RemoveAt(0);
 
-            // Once we're moving fast again we can let more bandwidth through
-            if (limit > 500) RunnerPenalty = 0;
+            // if (old != MaxRunners) Console.Write("." + MaxRunners.ToString()); // :T:
         }
 
-        public async Task<string> getJsonAsync(string path)
+        private async Task<string> getJsonAsync(string path)
         {
             // limit the number of concurrent api calls
             // 700 rate-limit, 50 credit-hold, 14 api calls at once reduce rate-limit to 0
-            while (runners >= MaxRunners)
+            while (Runners >= MaxRunners)
             {
                 System.Threading.Thread.Sleep(1); // cpu intensive but it's not doing anything else.
             }
 
-            runners += 1;
+            Runners += 1;
 
             // make the actual api call and wait for it
             // luckily this is threaded so other stuff can happen at the same time
-            var response = await client.GetAsync(api_context + path);
+            var response = await Client.GetAsync(APIContext + path);
             
             // done running so free up a spot for other threads
-            runners -= 1;
+            Runners -= 1;
             
             // check if the request was denied because we're over our API limit
             // then try until you succeed
+            var status = response.Headers.GetValues("Status").FirstOrDefault();
+            var retries = 1;
+
+            // keep trying until APIRetryLimit
+            // just 
+            while (status == "403 Forbidden")
+            {
+                // Console.Error.Write("*"); // :T:
+                ErrorCount += 1; // :T:
+
+                if (ShowRetries)
+                {
+                    Console.Error.Write(" | " + response.Headers.GetValues("Status").FirstOrDefault());
+                    Console.Error.Write(" | " + path);
+                    Console.Error.WriteLine(" | retrying " + retries);
+                }
+
+                System.Threading.Thread.Sleep(retries * 1000);
+
+                while (Runners >= MaxRunners) { System.Threading.Thread.Sleep(1); }
+                Runners += 1;
+                response = await Client.GetAsync(APIContext + path);
+                Runners -= 1;
+
+                status = response.Headers.GetValues("Status").FirstOrDefault();
+                var limit403 = response.Headers.GetValues("X-Rate-Limit-Remaining").FirstOrDefault();
+                RecalculateMaxRunners(BigCost * 4, float.Parse(limit403)); // extra expensive to slow things down
+
+                retries += 1;
+
+                if (status != "200 OK" && retries >= APIRetryLimit)
+                {
+                    Console.Error.WriteLine("");
+                    Console.Error.Write("too many " + status);
+                    Console.Error.WriteLine(" at: " + path);
+                    Console.Error.WriteLine("Probably an invalid call");
+                    Console.Error.WriteLine(response);
+
+                    return await response.Content.ReadAsStringAsync();
+                }
+            }
+
             if (response.Headers.GetValues("Status").FirstOrDefault() != "200 OK")
             {
-                // Console.Error.WriteLine($"Failed:{path}: ... Retrying...");
-                RunnerPenalty += 1;
-                System.Threading.Thread.Sleep(500);
-
-                return await getJsonAsync(path);
+                Console.Error.WriteLine("");
+                Console.Error.Write(response.Headers.GetValues("Status").FirstOrDefault());
+                Console.Error.WriteLine(" :at: " + path);
+                Console.Error.WriteLine(response);
+                return await response.Content.ReadAsStringAsync();
             }
             
             // parse some response
             var limit = response.Headers.GetValues("X-Rate-Limit-Remaining").FirstOrDefault();
             var cost = response.Headers.GetValues("X-Request-Cost").FirstOrDefault();
-
             RecalculateMaxRunners(float.Parse(cost), float.Parse(limit));
 
             var results = await response.Content.ReadAsStringAsync();
@@ -128,18 +182,6 @@ namespace CanvasAPIWrapper
         public async Task<string> Get(string api_call)
         {
             return await getJsonAsync(api_call);
-        }
-
-        public async Task<string> BigAPICall(string page_number)
-        {
-            string call = "accounts/1/courses?include[]=needs_grading_count&include[]=syllabus_body&include[]=public_description&include[]=total_scores&include[]=current_grading_period_scores&include[]=term&include[]=account&include[]=course_progress&include[]=sections&include[]=storage_quota_used_mb&include[]=total_students&include[]=passback_status&include[]=favorites&include[]=teachers&include[]=observed_users&include[]=course_image&include[]=concluded&per_page=100&page=";
-            return await getJsonAsync(call + page_number);
-        }
-
-        public async Task<string> SmallAPICall(string page_number)
-        {
-            string call = "accounts/1/courses?page=";
-            return await getJsonAsync(call + page_number);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,31 +12,66 @@ namespace APITesting
 {
     class Program
     {
-        static async Task Main(string[] args)
+        public static Task<string> BigAPICall(string page_number, Wrapper wrapper)
         {
+            string call = "accounts/1/courses?include[]=needs_grading_count&include[]=syllabus_body&include[]=public_description&include[]=total_scores&include[]=current_grading_period_scores&include[]=term&include[]=account&include[]=course_progress&include[]=sections&include[]=storage_quota_used_mb&include[]=total_students&include[]=passback_status&include[]=favorites&include[]=teachers&include[]=observed_users&include[]=course_image&include[]=concluded&per_page=100&page=";
+            return wrapper.http.Get(call + page_number);
+        }
+
+        public static Task<string> SmallAPICall(string page_number, Wrapper wrapper)
+        {
+            string call = "accounts/1/courses?page=";
+            return wrapper.http.Get(call + page_number);
+        }
+
+        static async Task<string> test()
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+
             var myhttp = new HttpClient();
             Wrapper canvas = new Wrapper(myhttp);
-
-            // keep track of all the tasks
             var myCourses = new List<Task<string>>();
 
-            for (int i = 1; i <= 400; i++)
+            // canvas.http.Show403Retries(true); // show everytime a course is retried
+
+            // expensive API calls
+            for (int i = 1; i <= 268; i++)
             {
-                myCourses.Add(canvas.http.BigAPICall(i.ToString()));
+                myCourses.Add(BigAPICall(i.ToString(), canvas));
             }
 
-            for (int i = 1; i <= 1000; i++)
+            // cheap API calls
+            for (int i = 1; i <= 1245; i++)
             {
-                myCourses.Add(canvas.http.SmallAPICall(i.ToString()));
+                myCourses.Add(SmallAPICall(i.ToString(), canvas));
             }
 
-            // wait until everything finishes before doing the next code
-            string[] results = await Task.WhenAll(myCourses.ToArray());
+            // these occur concurrently and out of order, so they break the API limiting system
+            // and cause interesting errors
 
-            Console.WriteLine("");
+            string[] results = await Task.WhenAll(myCourses.ToArray()); // wait for everything to finish
 
-            // this shows that things come back in the correct (expected) order
-            Array.ForEach(results, x => Console.Write(x.Count().ToString() + " | "));
+            // everything comes back in the expected order
+            // Console.WriteLine("");
+            // Array.ForEach(results, x => Console.Write((x.Count() % 10000).ToString().PadLeft(4) + " | "));
+
+            timer.Stop();
+            return timer.Elapsed + " - " + canvas.http.ErrorCount + "/" + myCourses.Count;
+        }
+
+        static async Task Main(string[] args)
+        {
+            List<string> results = new List<string>();
+            for (int i = 0; i < 10; i++)
+            {
+                results.Add(await test());
+            }
+            
+            foreach (var item in results)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
